d9457ab601383d0ae7aaff9151b38d4ec857117a
jkapl <joelkaplan1@gmail.com>
2019-05-30 15:59:01 -0700

    Complete addToTail and removeHead

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,21 +1,38 @@
-var LinkedList = function() {
+var LinkedList = function () {
   var list = {};
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value) {
+  list.addToTail = function (value) {
+    if (!list.head) {
+      const newNode = Node(value);
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      const newTail = Node(value);
+      list.tail.next = newTail;
+      list.tail = newTail;
+    }
   };
 
-  list.removeHead = function() {
+  list.removeHead = function () {
+    if (list.head) {
+      const oldHead = list.head;
+      list.head = list.head.next;
+      oldHead.next = null;
+      return oldHead.value;
+    } else {
+      return null;
+    }
   };
 
-  list.contains = function(target) {
+  list.contains = function (target) {
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function (value) {
   var node = {};
 
   node.value = value;
