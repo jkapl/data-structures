8052e56f9ed122bf05ce668f04a0d09f6250ea91
jkapl <joelkaplan1@gmail.com>
2019-06-01 15:13:27 -0700

    Pass all except collision

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,22 +1,33 @@
 
 
-var HashTable = function() {
+var HashTable = function () {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.insert = function (k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (_.contains(this._storage[index], k)) {
+    var indexOfK = this._storage[index].indexOf(k);
+    this._storage[index][indexOfK + 1] = v;
+  } else {
+    this._storage[index] = [k, v];
+  }
 };
 
-HashTable.prototype.retrieve = function(k) {
+HashTable.prototype.retrieve = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (_.contains(this._storage[index], k)) {
+    var indexOfK = this._storage[index].indexOf(k);
+    return this._storage[index][indexOfK + 1];
+  }
 };
 
-HashTable.prototype.remove = function(k) {
+HashTable.prototype.remove = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var indexOfK = this._storage[index].indexOf(k);
+  this._storage[index].splice(indexOfK, 2);
 };
-
 
 
 /*
