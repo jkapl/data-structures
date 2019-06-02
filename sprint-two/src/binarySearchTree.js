f24d99623c0c4600eafa0762e505072ad2a70d94
jkapl <joelkaplan1@gmail.com>
2019-06-01 17:32:28 -0700

    Implement insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,36 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function (value) {
+  var obj = {};
+
+  obj.value = value;
+  obj.left = null;
+  obj.right = null;
+
+  obj.insert = function (val, node) {
+    node = node || obj;
+    if (node.value < val) {
+      if (node.right) {
+        obj.insert(val, node.right);
+      } else {
+        node.right = BinarySearchTree(val);
+      }
+    } else {
+      if (node.left) {
+        obj.insert(val, node.left);
+      } else {
+        node.left = BinarySearchTree(val);
+      }
+    }
+  }
+
+  obj.contains = function (val) {
+
+  }
+
+  obj.depthFirstLog = function (cb) {
+
+  }
+
+  return obj;
 };
 
 
